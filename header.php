768ebf8cec58c9ed2c92e85efa5f31bb5e9fca59
Adeline Levionnois <64022509+ad-levionnois@users.noreply.github.com>
2020-05-30 18:31:28 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Blueasy Freebe">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Blueasy Freebe</title>
+  </head>
+
+  <body>
+  <header>
+    <div class="container-fluid p-0">
+      <nav class="navbar navbar-expand-lg ">
+        <div class="container mb-5">
+          <a class="navbar-brand title" href="#">BLU<span class="text-blue">E</span>ASY</a>
+            <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+              <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                  <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-item nav-link" href="#">Services</a>
+                  <a class="nav-item nav-link" href="#">Features</a>
+                  <a class="nav-item nav-link" href="#">Portfolio</a>
+                  <a class="nav-item nav-link" href="#">Contact</a>
+                  <a class="nav-item nav-link" href="#"></a>
+                </div>
+              </div>
+        </div>
+      </nav> </br>
+    <div class="container text-center mt-5">
+      <p class="text-banner">“I’m looking for the unexpected.</p></br>
+      <p class="text-banner">I’m looking for things I’ve never seen before.” </p>
+    </div>
+</header>
+<main>
+
+
+  <section class="blue">
+    <div class="container">
+    <section class="blue ">
+      <div class="services">
+      <h2>SERVICES</h2>
+        <div class="row">
+          <div class="col-lg-3">
+            <img src="icons/coffee.png" alt="coffee">
+              <h4>COFFEE</h4>
+                <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+          </div>
+          <div class="col-lg-3">
+            <img src="icons/instant.png" alt="instant">
+              <h4>INSTANT</h4>
+                <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+          </div>
+          <div class="col-lg-3">
+            <img src="icons/dslr.png" alt="serious">
+              <h4>SERIOUS</h4>
+                <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+          </div>
+          <div class="col-sm-3">
+            <img src="icons/frame.png" alt="frame">
+              <h4>FRAME</h4>
+                <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+
+  <section class="portfolio mb-5">
+    <div class="container ">
+    <nav class="navbar navbar-expand-lg bg-transparent">
+      <h2>PORTFOLIO</h2>
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="navbarNav2">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">ALL <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">PANORAMA</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">PORTRAITS</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">MACRO</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">JOURNAL</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+            </li>
+          </ul>
+        </div>
+    </nav>
+    <div class="img-portfolio">
+      <img class="mb-2" src="img/portfolio/image1.png" alt="image1">
+      <img class="mb-2" src="img/portfolio/image2.png" alt="image2">
+      <img class="mb-2" src="img/portfolio/image3.png" alt="image3">
+      <img class="mb-2" src="img/portfolio/image4.png" alt="image4">
+      <img class="mb-2" src="img/portfolio/image5.png" alt="image5">
+      <img class="mb-2" src="img/portfolio/image6.png" alt="image6">
+      <img class="mb-2" src="img/portfolio/image7.png" alt="image7">
+      <img class="mb-2" src="img/portfolio/image8.png" alt="image8">
+      </div>
+    </div>
+
+
+
+  <section class="video mt-5">
+    <div class="container ">
+      <div class="row">
+        <div class="col-lg-6 ">
+          <h2 class="text-white">Just default Section</h2>
+            <p class="text-white">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.
+            Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+            <a href="#" class="btn btn-primary bg-transparent">VISIST ME</a>
+        </div>
+
+        <div class="col-lg-6 d-flex justify-content-end">
+            <iframe width="300" height="250" src="https://www.youtube.com/watch?v=MW1eQZ_PEb4"></iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+<section class="twitter">
+  <div class="container">
+    <h2 class="text-white">TWITTER</h2>
+     <p class="text-white">Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake @cheesecake tootsie roll.</p>
+  </div>
+</section>
+
+<section class="fond-2">
+  <div class="container mb-5">
+    <h2 class="john text-white">WHO IS JOHN DOE?</h2>
+      <div class="row">
+        <div class="col-lg-6">
+          <p class="text-white">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p></br>
+
+          <p class="text-white"> Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+        </div>
+      <div class="col-lg-6">
+      <p class="text-white">Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.</p>
+
+      <img src="icons/icon_twitter.png" alt="twitter">
+      <img src="icons/google-plus.png" alt="google-plus">
+      <img src="icons/icon_dribbble.png" alt="dribbble">
+      <img src="icons/icon_coffee.png" alt="coffee">
+    </div>
+  </div>
+  </div>
+</section>
+<div class="Testimonials">
+  <div class="container bg-write">
+    <h2>TESTIMONIALS</h2>
+    <p>”</p>
+    <p>Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</P>
+    <p>-Walter White</p>
+  </div>
+</div>
+
+
+
+<div class="contact">
+  <div class="container">
+    <form class="mt-5">
+    <h2 class="text-white ">CONTACT</h2>
+  <div class="form-row d-flex align-content-center">
+    <div class="form-group col-md-6">
+      <label class="text-white">FULL NAME</label>
+      <input type="email" class="form-control" id="inputEmail4">
+    </div>
+  </div>
+ <div class="form-row d-flex align-content-center">
+    <div class="form-group col-md-6">
+      <label class="text-white">EMAIL</label>
+      <input type="email" class="form-control" id="inputEmail4">
+    </div>
+  </div>
+  <div class="form-group d-flex align-content-center">
+    <label class="text-white">MESSAGE</label>
+    <input type="text" class="form-control" id="message" placeholder="">
+  </div>
+  <button type="submit" class="btn btn-primary">MESSAGE</button>
+  </div>
+
+
+</form>
+  </div>
+</div>
+
+
+
+
+
+</div>
