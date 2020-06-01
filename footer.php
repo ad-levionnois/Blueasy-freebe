aa2142c38b758b15ba6035636b5921f806bd557b
Adeline Levionnois <64022509+ad-levionnois@users.noreply.github.com>
2020-06-01 16:31:46 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer>
-<div class="container foot">
+<div class="container foot pl-0">
+  <div class="row">
+    <div class="col-lg-6">
   <ul class="nav">
   <li class="nav-item">
     <a class="nav-link active" href="#">HOME</a>
@@ -19,10 +21,13 @@
   </li>
 </ul>
 </div>
-
+<div class="col-lg-6">
+  <p class="text-white d-flex justify-content-end">2013 ©</p>
+</div>
+</div>
+</div>
 </footer>
-
-
+</div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="description" content="Blueasy Freebe">
+    <meta name="viewport" content="width=320">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Blueasy Freebe</title>
@@ -10,43 +11,40 @@
 
   <body>
   <header>
-    <div class="container-fluid p-0">
+  <div class="container-fluid p-0">
       <nav class="navbar navbar-expand-lg ">
-        <div class="container mb-5">
+        <div class="container">
           <a class="navbar-brand title" href="#">BLU<span class="text-blue">E</span>ASY</a>
             <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
               <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                  <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                  <a class="nav-item nav-link" href="#">Services</a>
-                  <a class="nav-item nav-link" href="#">Features</a>
-                  <a class="nav-item nav-link" href="#">Portfolio</a>
-                  <a class="nav-item nav-link" href="#">Contact</a>
-                  <a class="nav-item nav-link" href="#"></a>
+                  <a class="nav-item nav-link pl-5 active" href="#">HOME <span class="sr-only">(current)</span></a>
+                  <a class="nav-item nav-link pl-5" href="#">SERVICES</a>
+                  <a class="nav-item nav-link pl-5" href="#">FEATURES</a>
+                  <a class="nav-item nav-link pl-5" href="#">PORTFOLIO</a>
+                  <a class="nav-item nav-link pl-5" href="#">CONTACT</a>
                 </div>
               </div>
         </div>
-      </nav> </br>
-    <div class="container text-center mt-5">
-      <p class="text-banner">“I’m looking for the unexpected.</p></br>
-      <p class="text-banner">I’m looking for things I’ve never seen before.” </p>
+      </nav>
+    <div class="container-banner text-center">
+      <p class="text-banner">“I’M LOOKING FOR THE UNEXPECTED.</p>
+      <p class="text-banner">I’M LOOKING FOR THINGS I’VE NEVER SEEN BEFORE.” </p>
     </div>
 </header>
 <main>
 
 
-  <section class="blue">
+  <section class="services">
     <div class="container">
-    <section class="blue ">
-      <div class="services">
       <h2>SERVICES</h2>
-        <div class="row">
+        <div class="row icons ">
           <div class="col-lg-3">
             <img src="icons/coffee.png" alt="coffee">
               <h4>COFFEE</h4>
-                <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                <p >Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
           </div>
           <div class="col-lg-3">
             <img src="icons/instant.png" alt="instant">
@@ -64,7 +62,6 @@
                 <p>Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
           </div>
         </div>
-      </div>
     </div>
   </section>
 
@@ -73,7 +70,7 @@
 
 
 
-  <section class="portfolio mb-5">
+  <section class="portfolio ">
     <div class="container ">
     <nav class="navbar navbar-expand-lg bg-transparent">
       <h2>PORTFOLIO</h2>
@@ -105,20 +102,21 @@
         </div>
     </nav>
     <div class="img-portfolio">
-      <img class="mb-2" src="img/portfolio/image1.png" alt="image1">
-      <img class="mb-2" src="img/portfolio/image2.png" alt="image2">
-      <img class="mb-2" src="img/portfolio/image3.png" alt="image3">
-      <img class="mb-2" src="img/portfolio/image4.png" alt="image4">
-      <img class="mb-2" src="img/portfolio/image5.png" alt="image5">
-      <img class="mb-2" src="img/portfolio/image6.png" alt="image6">
-      <img class="mb-2" src="img/portfolio/image7.png" alt="image7">
-      <img class="mb-2" src="img/portfolio/image8.png" alt="image8">
+      <img class="" src="img/portfolio/image1.png" alt="image1">
+      <img class="" src="img/portfolio/image2.png" alt="image2">
+      <img class="" src="img/portfolio/image3.png" alt="image3">
+      <img class="" src="img/portfolio/image4.png" alt="image4">
+      <img class="" src="img/portfolio/image5.png" alt="image5">
+      <img class="" src="img/portfolio/image6.png" alt="image6">
+      <img class="" src="img/portfolio/image7.png" alt="image7">
+      <img class="" src="img/portfolio/image8.png" alt="image8">
       </div>
     </div>
+  </section>
 
 
 
-  <section class="video mt-5">
+  <section class="Just_default">
     <div class="container ">
       <div class="row">
         <div class="col-lg-6 ">
@@ -128,76 +126,97 @@
             <a href="#" class="btn btn-primary bg-transparent">VISIST ME</a>
         </div>
 
-        <div class="col-lg-6 d-flex justify-content-end">
-            <iframe width="300" height="250" src="https://www.youtube.com/watch?v=MW1eQZ_PEb4"></iframe>
+        <div class="col-lg-6">
+            <video width="300" height="250" src="https://www.youtube.com/watch?v=MW1eQZ_PEb4"></video>
         </div>
       </div>
     </div>
   </section>
-
+</div>
 
 <section class="twitter">
   <div class="container">
+    <div class="row padding-twitter">
+    <div class="col-lg-11">
     <h2 class="text-white">TWITTER</h2>
-     <p class="text-white">Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake @cheesecake tootsie roll.</p>
+      <p class="text-white">Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy.Chocolate oat cake </br>
+       <span class="cheesecake">@cheesecake</span> tootsie roll.</p>
+     </div>
+     <div class="col-lg-1">
+        <img class=""src="icons/twitter.png" alt="twitter">
+     </div>
+     </div>
   </div>
 </section>
 
-<section class="fond-2">
+<section class="fond-2 mt-0">
   <div class="container mb-5">
     <h2 class="john text-white">WHO IS JOHN DOE?</h2>
-      <div class="row">
+      <div class="row align-items-center">
         <div class="col-lg-6">
           <p class="text-white">Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com. Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.</p></br>
 
-          <p class="text-white"> Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p>
+          <p class="text-white"> Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.</p></br>
         </div>
       <div class="col-lg-6">
-      <p class="text-white">Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.</p>
+      <p class="text-white">Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.</p> </br>
 
-      <img src="icons/icon_twitter.png" alt="twitter">
-      <img src="icons/google-plus.png" alt="google-plus">
-      <img src="icons/icon_dribbble.png" alt="dribbble">
-      <img src="icons/icon_coffee.png" alt="coffee">
+      <img class="icons" src="icons/icon_twitter.png" alt="twitter">
+      <img class="icons"src="icons/google_plus.png" alt="google-plus">
+      <img class="icons"src="icons/icon_dribbble.png" alt="dribbble">
+      <img class="icons"src="icons/icon_coffee.png" alt="coffee">
     </div>
   </div>
   </div>
 </section>
-<div class="Testimonials">
-  <div class="container bg-write">
-    <h2>TESTIMONIALS</h2>
-    <p>”</p>
-    <p>Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</P>
-    <p>-Walter White</p>
+<div class="Testimonials  ">
+    <div class="container bg-write align-items-center ">
+      <div class="row">
+      <div class="col-lg-11">
+        <h2 class=>TESTIMONIALS</h2>
+          <p class="" >Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie soufflé sweet roll sweet roll jelly-o.</P>
+            <cite>-Walter White</cite>
+      </div>
+      <div class="col-lg-1">
+        <p class="guillemet">”</p>
+      </div>
+    </div>
   </div>
 </div>
 
-
-
 <div class="contact">
   <div class="container">
-    <form class="mt-5">
+      <div class="row">
+    <div class="col-lg-6">
+    <form>
     <h2 class="text-white ">CONTACT</h2>
   <div class="form-row d-flex align-content-center">
     <div class="form-group col-md-6">
       <label class="text-white">FULL NAME</label>
-      <input type="email" class="form-control" id="inputEmail4">
+      <input type="email" class="form-control">
     </div>
   </div>
- <div class="form-row">
+ <div class="form-row d-flex align-content-center">
     <div class="form-group col-md-6">
       <label class="text-white">EMAIL</label>
-      <input type="email" class="form-control" id="inputEmail4">
+      <input type="email" class="form-control">
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group d-inline-block align-content-center">
     <label class="text-white">MESSAGE</label>
-    <input type="text" class="form-control" id="message" placeholder="">
+    <input type="text" class="form-control" id="message">
   </div>
-  <button type="submit" class="btn btn-primary">MESSAGE</button>
+  <button type="submit" class="btn btn-primary ">MESSAGE</button>
+  </form>
   </div>
 
-
-</form>
-  </div>
+    <div class="col-lg-6 text-contact">
+      <p class="text-white">Jujubes bear claw croissant wafer sweet lemon drops brownie chocolate bar. Sweet lollipop halvah tootsie roll cake fruitcake. Chocolate cake chocolate bar gingerbread lollipop tart. Jujubes bear claw donut chupa chups.</p>
+      <p class="text-white">Linda Newman</br>
+W325 State Road 123</br>
+Mondovi, WI(Wisconsin) 98746-54321</br>
+(715) 946 - 1234<p>
+    </div>
 </div>
+</div>
+</main>
